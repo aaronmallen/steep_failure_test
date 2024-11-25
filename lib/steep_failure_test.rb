06f82050779e82286267011499bd31dd18a119f9
Aaron Allen <aaron.allen@joinroot.com>
2024-11-25 17:53:35 -0600

    Run `bundle gem steep_failure_test`

---
+++ lib/steep_failure_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "steep_failure_test/version"
+
+module SteepFailureTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/steep_failure_test/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SteepFailureTest
+  VERSION = "0.1.0"
+end
